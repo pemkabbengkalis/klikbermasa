04dfb43d9d3c09b12dbf7c2cc442e9c198cb641b
Muhammad Nurul Hudin <m.nurul.hudin@gmail.com>
2025-01-21 14:55:57 +0700

    IP DETEKTOR

--- app/Http/Middleware/WebMiddleware.php
+++ app/Http/Middleware/WebMiddleware.php
@@ -17,7 +17,7 @@ class WebMiddleware
     public function handle(Request $request, Closure $next): Response
     {
 
-        if(in_array(request()->ip(), [':::1','127.0.0.1'])){
+        if(!in_array(request()->ip(), [':::1','127.0.0.1'])){
         if(Route::currentRouteName() == 'login' && $request->getHost() == api_url()){
             return response()->json(['code'=>'403','status'=>'User Unauthorized']);
         }
